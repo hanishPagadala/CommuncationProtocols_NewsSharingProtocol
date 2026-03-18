3552995fd9da3d3a38459cdbbc49d24c591cbcde
hanishPagadala <hanishpagadala05@gmail.com>
2026-03-17 22:47:52 -0400

    First Comit

---
+++ Client.py
@@ -0,0 +1,121 @@
+import socket
+import sys
+import time
+import errno
+import threading
+
+udpHOST = "0.0.0.0"
+PORTNo = 8889
+
+udpServerHost = 'localhost'
+udpServerPort = 8888
+
+try:
+    udpSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+except socket.error as msg:
+    print('Failed to create socket. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
+    sys.exit()
+# try:
+#     udpSock.bind((udpHOST, PORTNo))
+# except socket.error as msg:
+#     print('Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
+#     sys.exit()
+
+Request = 0
+Name = "Lebron"
+clientIP = socket.gethostbyname(socket.gethostname())
+server_address = ('localhost', 10000)
+
+def sendMessage(message):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect(server_address)
+    print("Connected to server at", server_address)
+
+    sock.sendall(message.encode())
+    print("Sending Message to Server")
+
+    recieved = ""
+    data = sock.recv(1024)
+    if data:
+        recieved += data.decode()
+        print("Received:", recieved)
+
+    print("no more info to receive from the server at", server_address)
+    sock.close()
+    time.sleep(0.2)
+
+def sendUDPMessage(message):
+    udpSock.sendto(message.encode(), (udpServerHost, udpServerPort))
+    print("Sending Message to Server UDP")
+
+    data = udpSock.recvfrom(4096)
+    reply = data[0].decode()
+    addr = data[1]
+
+    print("Received:", reply, "from", addr)
+    time.sleep(0.2)
+
+try:
+    while True:
+        time.sleep(0.1) 
+        message = ""
+        userAction = input("Choose an Action: Register | Unregister | Update | Subjects | Publish | Quit: ")
+        userAction = userAction.lower()
+        messageType = ""
+        if (userAction == "register"):
+
+            userName = input("Enter your name: ")
+            message = "Register " + str(Request) + " " + userName + " " + clientIP + " " + str(PORTNo)
+            messageType = "TCP"
+
+        elif userAction == "unregister":
+
+            userName = input("Enter your name: ")
+            message = "Unregister " + str(Request) + " " + userName
+            messageType = "TCP"
+
+        elif userAction == "update":
+            userName = input("Enter your name: ")
+            message = "Update " + str(Request) + " " + userName
+            messageType = "TCP"
+
+        elif userAction == "subjects":
+            
+            userName = input("Enter your name: ")
+            listOfSubjects = input("Enter the subjects you want to subscribe to (space separated): ")
+            message = "Subjects " + str(Request) + " " + userName + " " + listOfSubjects
+            messageType = "TCP"
+
+        elif userAction == "quit":
+
+            message = "Quit"
+            messageType = "TCP"
+
+        elif userAction == "publish":
+
+            userName = input("Enter your name: ")
+            userSubject = input("Enter the subject you wish to publish: ")
+            subjectTitle = input("Enter the title of your publication: ")
+            subjectText = input("Enter the text of your publication: ")
+            message = "Publish " + str(Request) + " " + userName + " "+ userSubject + " Titl3:" + subjectTitle + " T3xt:" + subjectText
+            messageType = "UDP"
+
+        else:
+            message = "Penis"
+            messageType = "TCP"
+        
+        if messageType == "TCP":
+            tcpThread = threading.Thread(target=sendMessage, args=(message, )) 
+            tcpThread.start()
+            tcpThread.join()
+        elif messageType == "UDP":
+            udpThread = threading.Thread(target=sendUDPMessage, args=(message, )) 
+            udpThread.start()
+            udpThread.join()
+    
+        if userAction == "quit":
+            break
+        Request += 1
+finally:
+    time.sleep(0.5)
+    udpSock.close()
---
+++ Server.py
@@ -0,0 +1,152 @@
+import socket
+import threading
+import sys
+import time
+import csv
+
+HOST = 'localhost'
+PORT = 10000
+
+UDPHOST = "0.0.0.0"
+UDPPORT = 8888
+
+try:
+    udpSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+except socket.error as msg:
+    print('Failed to create socket. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
+    sys.exit()
+try:
+    udpSock.bind((UDPHOST, UDPPORT))
+except socket.error as msg:
+    print('Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
+    sys.exit()
+
+clientThreads = []
+udpThread = None
+ResgisterdClients = []
+clientSubjects = []
+
+def writeToCSV(userMessage):
+    with open('../registeredClient.csv', mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['Client Name', 'IP Address', 'UDP Port'])
+        for client in ResgisterdClients:
+            writer.writerow(client[0:3])
+    print("Client information written to registeredClient.csv")
+
+
+
+def getDatafromClient(connection, client_address):
+    print('connection from %s' % (client_address,), file=sys.stderr)
+    try:
+        while True:
+            data = connection.recv(4096)
+            print(f'received {data}', file=sys.stderr)
+
+            if not data:
+                print('no more data from', client_address, file=sys.stderr)
+                break
+
+            request = data.decode().strip()
+            if not request:
+                continue
+
+            command = request.split()[0]
+            if command == "Register":
+                parts = request.split()
+
+                if len(parts) < 2:
+                    message = "UNABLE TO REGISTER: INVALID REQUEST FORMAT"
+                else:
+                    request_id = parts[1]
+                    if len(parts) < 3:
+                        message = f"REGISTER DENIED: RQ: {request_id} CLIENT NAME NOT PROVIDED"
+                    else:
+                        client_name = parts[2]
+                        if any(client[0] == client_name for client in ResgisterdClients):
+                            message = f"REGISTER DENIED: RQ: {request_id} ALREADY REGISTERED"
+                        else:
+                            ResgisterdClients.append((parts[2], parts[3], parts[4]))
+                            message = f"REGISTERED {request_id}"
+                            if len(parts) >= 5:
+                                writeToCSV(parts)
+       
+                connection.sendall(message.encode())
+            elif command == "Unregister":
+                message = "UNREGISTERED " + request.split()[1]
+                for client in ResgisterdClients:
+                    if client[0] == request.split()[2]:
+                        ResgisterdClients.remove(client)
+                        break
+                else:
+                    message = "NOT REGISTERED"
+                connection.sendall(message.encode())
+            elif command == "Update":
+                message = "UPDATE-CONFIRMED " + request.split()[1] + " " + request.split()[2] + " " + client_address[0]
+                for client in ResgisterdClients:
+                    if client[0] == request.split()[2]:
+                        ResgisterdClients.remove(client)
+                        ResgisterdClients.append((request.split()[2], client_address))
+                        break
+                else:
+                    message = "UPDATE-DENIED " + request.split()[1] + " Name not registered"
+                connection.sendall(message.encode())
+
+            elif command == "Subjects":
+                counter = 0
+                print(request)
+                response = ''
+                for item in request.split():
+                    print(item)
+                    counter += 1
+                    if counter > 3:
+                        clientSubjects.append(item)
+                        response += item + " "
+
+                print(clientSubjects)
+
+                message = "SUBJECTS UPDATED " + response
+                
+                connection.sendall(message.encode())
+            elif command == "Quit":
+                break
+            else:
+                message = "INVALID COMMAND"
+                connection.sendall(message.encode())
+    finally:
+        print('closing connection to', client_address, file=sys.stderr, flush=True)
+        connection.close()
+    
+def getUDPDataFromClient():
+    try:
+        while True:
+            data = udpSock.recvfrom(4096)
+            message = data[0].decode()
+            addr = data[1]
+            print(f"Received {message} from {addr}")
+            
+            udpSock.sendto(message.encode(), addr)
+    finally:
+        time.sleep(0.2)
+        udpSock.close()
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_address = (HOST, PORT)
+print(f'starting up on {HOST} port {PORT}', file=sys.stderr)
+sock.bind(server_address)
+
+# Server starts listening on the port
+sock.listen(5)
+
+# Start the UDP listener once, as a daemon so it doesn't block shutdown
+udpThread = threading.Thread(target=getUDPDataFromClient, daemon=True)
+udpThread.start()
+
+while True:
+    # Wait for a connection
+    print('waiting for a connection', file=sys.stderr, flush=True)
+
+    connection, client_address = sock.accept()
+    handleClientThread = threading.Thread(target=getDatafromClient, args=(connection, client_address,))
+    handleClientThread.start()
+    clientThreads.append(handleClientThread)
